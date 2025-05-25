473d2f43aadd6476caecd5163d3e3110dca32157
Stéphane Kuma <kumastephane@gmail.com>
2025-05-25 20:35:40 +0000

    feat: add tenant model & relation

---
+++ app/Models/Store.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Store extends Model
+{
+    /** @use HasFactory<\Database\Factories\StoreFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'location',
+        'data',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'data' => 'array',
+        ];
+    }
+
+    /**
+     * The users that belong to the Store
+     *
+     * @return BelongsToMany
+     */
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'store_user', 'store_id', 'user_id');
+    }
+}
