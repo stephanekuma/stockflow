fcfa91f14a571ba22a1f9fb25e268f864ef01088
Stéphane Kuma <kumastephane@gmail.com>
2025-05-25 21:54:40 +0000

    feat: add the Setting model, factory and seeder, as well as the migration for the settings table

---
+++ app/Models/Setting.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Setting extends Model
+{
+    /** @use HasFactory<\Database\Factories\SettingFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'key',
+        'value',
+        'group',
+        'attributes',
+        'type',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'attributes' => 'array',
+        ];
+    }
+}
---
+++ database/factories/SettingFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Setting>
+ */
+class SettingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'key' => $this->faker->unique()->word(),
+            'value' => $this->faker->text(),
+            'group' => $this->faker->word(),
+            'attributes' => [],
+            'type' => 'text',
+        ];
+    }
+}
