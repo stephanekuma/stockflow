67976a404320fd6452bc8dfde297484cb52f82fa
Stéphane Kuma <kumastephane@gmail.com>
2025-05-30 00:25:20 +0000

    feat: add Brand resource with create, edit, and list pages

---
+++ app/Filament/Resources/BrandResource.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BrandResource\Pages;
+use App\Filament\Resources\BrandResource\RelationManagers;
+use App\Models\Brand;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BrandResource extends CategoryResource
+{
+    protected static ?string $model = Brand::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function getNavigationGroup(): string
+    {
+        return __('Products Management');
+    }
+
+    public static function getModelLabel(): string
+    {
+        return __('Brand');
+    }
+
+    public static function getPluralModelLabel(): string
+    {
+        return __('Brands');
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBrands::route('/'),
+            // 'create' => Pages\CreateBrand::route('/create'),
+            // 'edit' => Pages\EditBrand::route('/{record}/edit'),
+        ];
+    }
+}
